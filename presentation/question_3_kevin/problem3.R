efad5015bdf413f4b9c505b6e7cf1db4a04bf8ee
kevinwjin <kevin.jin@utdallas.edu>
2022-03-25 00:15:41 -0400

    Add question 3 files

--- presentation/question_3_kevin/problem3.R
+++ presentation/question_3_kevin/problem3.R
@@ -6,36 +6,64 @@ library(ggplot2)
 library(dplyr)
 library(lubridate)
 
-# Load data
-cases <- read.csv("time_series_covid19_confirmed_US.csv")
+# Load Texas cases data, extract daily cases
+cases_tx <- read.csv("time_series_covid19_confirmed_US.csv") %>% 
+  filter(Province_State == "Texas") %>%
+  select(starts_with("X")) 
 
-cases_tx <- cases %>% filter(Province_State == "Texas") # subset by Texas
+# Transpose data frame
+cases <- as.data.frame(t(cases_tx))
 
-sums <- colSums(select(cases_tx, starts_with("X"))) # sum all case columns by day
-sums <- as.data.frame((sums)) # convert to single column data frame
-dates <- rownames(sums) # extract dates
-sums <- mutate(sums, Date = dates) # add back as Date column
-rownames(sums) <- NULL # delete row names
-names(sums) <- c("Cases", "Date") # rename columns
-sums <- sums[ , c("Date", "Cases")] # switch first and second columns
-sums$Date <- sub('^X', '', sums$Date) # regex to remove X from all dates
-sums$Date <- mdy(sums$Date) # convert dates to Date format
+# Add total column for daily Texas cases across all counties
+cases <- cases %>% mutate(total = rowSums(cases))
 
-# Extract cases per day for whole US (non-cumulative)
-cases_new <- cases %>% 
-  select(Province_State, Confirmed, Deaths) %>%
-  rename(State = Province_State)
+# Add daily column for non-cumulative daily cases across all counties
+cases$daily <- diff(c(0, cases$total))
 
-cases_new$State <- factor(cases_new$State)
+# Create date column
+dates <- rownames(cases)
+dates <- sub('^X', '', dates)
+dates <- mdy(dates)
+cases$date <- dates
 
-# Plot time series and fit linear regression
-ggplot(data = cases_new, mapping = aes(x = reorder(State, Confirmed), 
-                                       y = Confirmed,
-                                       fill = Deaths)) + 
-  geom_bar(stat = "identity", position = "stack") +
-  xlab("State") +
-  ylab("Confirmed Cases") +
-  ggtitle("Confirmed Cases by State") +
-  coord_flip() 
+# Draw plots
+ggplot(data = cases, mapping = aes(x = dates, y = daily)) + 
+  geom_point() + 
+  geom_smooth(method = "loess") + 
+  labs(x = "Date", y = "Cases", 
+       title = "Time Series Trend of Daily Cases in Texas")
+ggsave("daily_cases.png")
 
-# Extract vaccine administrations per day (non-cumulative)
+ggplot(data = cases, mapping = aes(x = dates, y = total)) + 
+  geom_point() + 
+  geom_smooth(method = "lm") + 
+  labs(x = "Date", y = "Cases", 
+       title = "Time Series Trend of Total Cases in Texas")
+ggsave("total_cases.png")
+
+# Load Texas vaccine data
+#vax <- read.csv("time_series_covid19_vaccine_doses_admin_US.csv")
+vax <- read.csv("vaccine_data_us_timeline.csv")
+
+# Extract daily vaccine administrations
+vax$Date <- ymd(vax$Date) # convert Date column to Date format
+vax_tx <- vax %>%
+  filter(Province_State == "Texas" & # extract Texas and All vaccine type rows
+           Vaccine_Type == "All") %>% 
+  select(Date, Doses_admin) %>% # extract Date and Doses Administered variables
+  mutate(Doses_millions = Doses_admin/1000000) %>% # display doses by millions
+  mutate(Doses_admin_daily = diff(c(0, vax_tx$Doses_admin)))
+
+ggplot(data = vax_tx, mapping = aes(x = Date, y = Doses_admin_daily)) + 
+  geom_point() + 
+  geom_smooth(method = "loess") + 
+  labs(x = "Date", y = "Doses Administered", 
+       title = "Time Series Trend of Daily Vaccines Administered in Texas")
+ggsave("daily_vax.png")
+
+ggplot(data = vax_tx, mapping = aes(x = Date, y = Doses_millions)) + 
+  geom_point() + 
+  geom_smooth(method = "lm") + 
+  labs(x = "Date", y = "Doses Administered (in millions)", 
+       title = "Time Series Trend of Total Vaccines Administered in Texas")
+ggsave("total_vax.png")
